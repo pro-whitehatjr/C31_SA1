806f23b4ad22ed439ce1d2423acda7837ce8c14e
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-04-14 18:59:08 +0530

    Add files via upload

---
+++ link.js
@@ -0,0 +1,22 @@
+class Link{
+    constructor(bodyA,bodyB)
+    {
+     this.link = Constraint.create(
+        {
+          bodyA:bodyA.body.bodies[6],
+          pointA:{x:0,y:0},
+          bodyB:bodyB,
+          pointB:{x:0,y:0},
+          length:-10,
+          stiffness:0.01
+        });
+        World.add(engine.world,this.link);
+    } 
+
+    dettach()
+    {
+      World.remove(engine.world,this.link);
+     
+    }
+}
+
---
+++ sketch.js
@@ -0,0 +1,95 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var rope,fruit,ground;
+var fruit_con;
+var fruit_con_2;
+
+var bg_img;
+var food;
+var rabbit;
+
+var button;
+var bunny;
+var blink,eat,sad;
+
+function preload()
+{
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');;
+  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+  
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+  sad.looping= false;
+  eat.looping = false; 
+}
+
+function setup() {
+  createCanvas(500,700);
+  frameRate(80);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+  button = createImg('cut_btn.png');
+  button.position(220,30);
+  button.size(50,50);
+  button.mouseClicked(drop);
+  
+  rope = new Rope(7,{x:245,y:30});
+  ground = new Ground(200,690,600,20);
+
+  bunny = createSprite(230,620,100,100);
+  bunny.scale = 0.2;
+
+  bunny.addAnimation('blinking',blink);
+
+  bunny.addAnimation('eating',eat);
+  bunny.addAnimation('crying',sad);
+  bunny.changeAnimation('blinking');
+  
+  fruit = Bodies.circle(300,300,20);
+  Matter.Composite.add(rope.body,fruit);
+
+  fruit_con = new Link(rope,fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  imageMode(CENTER);
+  
+}
+
+function draw() 
+{
+  background(51);
+  image(bg_img,width/2,height/2,490,690);
+
+  image(food,fruit.position.x,fruit.position.y,70,70);
+
+  rope.show();
+  Engine.update(engine);
+  ground.show();
+
+   drawSprites();
+}
+
+function drop()
+{
+  rope.break();
+  fruit_con.dettach();
+  fruit_con = null; 
+}
+
