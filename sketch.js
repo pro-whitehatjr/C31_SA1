c4ffb105fea87de8cbdd4ecc607e9801b299aa8f
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-18 18:23:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,7 +60,7 @@ function setup() {
   bunny.addAnimation('crying',sad);
   bunny.changeAnimation('blinking');
   
-  rope = new Rope(6,{x:245,y:30});
+  rope = new Rope(7,{x:245,y:30});
   ground = new Ground(200,690,600,20);
   
   fruit = Bodies.circle(300,300,20);
