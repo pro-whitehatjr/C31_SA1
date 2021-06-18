9b294a5196dd5a6d397e3f9add1d0f5d39930402
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-18 18:19:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -49,17 +49,19 @@ function setup() {
   button.size(50,50);
   button.mouseClicked(drop);
   
-  rope = new Rope(7,{x:245,y:30});
-  ground = new Ground(200,690,600,20);
 
+  blink.frameDelay = 20;
+  eat.frameDelay = 20;
   bunny = createSprite(230,620,100,100);
   bunny.scale = 0.2;
 
   bunny.addAnimation('blinking',blink);
-
   bunny.addAnimation('eating',eat);
   bunny.addAnimation('crying',sad);
   bunny.changeAnimation('blinking');
+  
+  rope = new Rope(6,{x:245,y:30});
+  ground = new Ground(200,690,600,20);
   
   fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
@@ -89,7 +91,7 @@ function draw()
 function drop()
 {
   rope.break();
-  fruit_con.dettach();
+  fruit_con.detach();
   fruit_con = null; 
 }
 
